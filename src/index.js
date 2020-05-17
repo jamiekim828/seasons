7325bcd7e61f15bb981948038dac67bb930d589b
jamiekim828 <jamiekim828@gmail.com>
2020-05-17 23:47:48 +0200

    geolocation setting

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import ReactDOM from 'react-dom';
 
 // App Challenges
 // -Need to get the users physical location
+// (Geolocation API)
+// https://developer.mozilla.org/en-US/docs/Web/API/geolocation_API
 // -Need to determine the current month
 // -Need to change text and styling based on location + month
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  );
   return <div>Hi there!</div>;
 };
 
