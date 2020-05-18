0be94e46c3a11c36f6c2323a1c57b74030ea2429
jamiekim828 <jamiekim828@gmail.com>
2020-05-18 10:14:48 +0200

    get season in SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat < 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = (props) => {
   console.log(props.lat);
+  //new Date().getMonth()
+  //consider 0 index
+  //so 4 means May
+  //Northern Hemisphere: 3~8 hot, Southern Hemispdere: 9, 10, 11, 0, 1, 2 hot
+
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+
   return <div>Season Display</div>;
 };
 
